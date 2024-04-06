26cb0b8ea52783c7e2b952a38d5bb0e0bf2fefe5
Zhuk Aleksandr <jobbox.zh@gmail.com>
2024-04-06 20:36:42 +0300

    routes

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,18 +1,25 @@
 import ResultBlock from './components/ResultBlock';
 import Finish from './components/Finish';
+import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<ResultBlock />} />
-        <Route path={'/finish'} element={<Finish />} />
-      </Routes>
-    </BrowserRouter>
+      <>
+        <header className='App-header'>
+          <Link to="/">Home</Link>
+          <Link to="/finish">Finish</Link>
+        </header>
+        <div className='App-body'>
+        <h1>Main part</h1>
+          <Routes>
+            
+            <Route path={'/'} element={<ResultBlock />} />
+            <Route path={'/finish'} element={<Finish />} />
+          </Routes>
+        </div>
+      </>
   );
 }
 
