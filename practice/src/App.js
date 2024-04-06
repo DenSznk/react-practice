95edac075096ad44b7fe1d1c5e86734951573de7
Zhuk Aleksandr <jobbox.zh@gmail.com>
2024-04-06 21:13:45 +0300

    navLink

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,20 +1,27 @@
 import ResultBlock from './components/ResultBlock';
 import Finish from './components/Finish';
 import './App.css';
-
+import { NavLink } from "react-router-dom";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-      <>
+      <>      
         <header className='App-header'>
-          <Link to="/">Home</Link>
-          <Link to="/finish">Finish</Link>
-        </header>
+
+        <NavLink to="/" className={({ isActive }) => isActive ? 'active-link' : undefined}>
+              Home
+          </NavLink>
+          <NavLink 
+            to="/finish"
+            className={({ isActive }) => isActive ? 'active-link' : undefined}>Finish
+            </NavLink>
+
+        </header>     
+        
         <div className='App-body'>
         <h1>Main part</h1>
-          <Routes>
-            
+          <Routes>            
             <Route path={'/'} element={<ResultBlock />} />
             <Route path={'/finish'} element={<Finish />} />
           </Routes>
